1d2d3668308cd6afe0167f1d4d1b20873f902f88
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-10-25 15:53:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,11 +9,13 @@ GPIO.setup(26, GPIO.OUT)
 
 time.sleep(3)
 #19 = Motor1 backwars 
+#21 = Motor1 forwards
+
 
 GPIO.output(19, 0)
 GPIO.output(21, 1)
 
-GPIO.output(24, 0)
+GPIO.output(24, 1)
 GPIO.output(26, 0)
 
 time.sleep(3)
