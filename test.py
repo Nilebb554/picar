ff6161bb69c5098ed2a91a8880c274ef78d5a41c
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-10-25 14:26:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,9 @@ import time
 
 robot = Robot(left=(24, 26), right=(19, 21))
 
-GPIO
 def test():
     robot.forward()
     time.sleep(3)
-    robot.backward()
-    time.sleep(3)
-    robot.left()
-    time.sleep(3)
-    robot.right()
-    time.sleep(3)
-    robot.stop()
     print("DONE")
 
 test()
