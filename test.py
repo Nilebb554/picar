d56bab77c7fae38f24a10599fb8c7baf74223dc3
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-10-25 14:39:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,11 @@ time.sleep(5)
 
 motor1 = Motor(21, 26)
 motor2 = Motor(19, 24)
-
+print("1")
 def test():
+    print("2")
     motor1.forward()
+    print("3")
     time.sleep(3)
     print("DONE")
 
