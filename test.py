297f5bcac088a03cde5e015d5931a7bdc5d6a76b
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-10-25 14:19:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,76 +1,26 @@
 from gpiozero import Robot
 import time
 
-robot = Robot((21, 26),(19, 24))
+robot = Robot(left=(21, 26), right=(19, 24))
 
 def test():
-  robot.forward()
-  time.sleep(3)
-  robot.backward()
-  time.sleep(3)
-  robot.left()
-  time.sleep(3)
-  robot.right()
-  time.sleep(3)
-  robot.stop()
-  print("DONE")
+    robot.forward()
+    time.sleep(3)
+    robot.backward()
+    time.sleep(3)
+    robot.left()
+    time.sleep(3)
+    robot.right()
+    time.sleep(3)
+    robot.stop()
+    print("DONE")
 
 test()
-
-#!/usr/bin/env python
-# Simply sets motors on and off in both directions
-# Motor A Pins 21 and 26
-# Motor B Pins 19 and 24
-
-import time, RPi.GPIO as GPIO
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(19, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
-
-# wait after setup to check nothing happens!
-time.sleep(3)
-
-# switch MotorA on forwards
-GPIO.output (21, 0)
-GPIO.output (26, 1)
-
-# wait another 3 seconds
-time.sleep(3)
-
-# switch MotorA on backwards
-GPIO.output (21, 1)
-GPIO.output (26, 0)
-
-# wait another 3 seconds
-time.sleep(3)
-
-# switch MotorA off
-GPIO.output (21, 0)
-GPIO.output (26, 0)
-
-# wait another 3 seconds
-time.sleep(3)
-
-# switch MotorB on Forwards
-GPIO.output (19, 0)
-GPIO.output (24, 1)
-
-# wait another 3 seconds
-time.sleep(3)
-
-# switch MotorB on Backwards
-GPIO.output (19, 1)
-GPIO.output (24, 0)
-
-# wait another 3 seconds
-time.sleep(3)
-
-# switch MotorB off
-GPIO.output (19, 0)
-GPIO.output (24, 0)
-
-GPIO.cleanup()
+test()
+test()
+test()
+test()
+test()
+test()
+test()
+test()
