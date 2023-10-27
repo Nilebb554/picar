d7b538d37c48ef5fc47b3b0fc6f0c3e5f3302891
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-10-27 14:41:34 +0000

    move with input

--- car_control.py
+++ car_control.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-import keyboard
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -45,36 +44,14 @@ def move_backward():
     GPIO.output(26, 0)
     GPIO.output(24, 1)
 
-#When left arrow down
-def on_left_arrow(event):
-    if event.event_type == keyboard.KEY_DOWN:
+while True:
+    move = input("Where move")
+    if move == "left":
         move_left()
-
-#When right arrow down
-def on_right_arrow(event):
-    if event.event_type == keyboard.KEY_DOWN:
+    elif move == "right":
         move_right()
-
-#When up arrow down
-def on_up_arrow(event):
-    if event.event_type == keyboard.KEY_DOWN:
+    elif move == "forward":
         move_forward()
-
-#When down arrow down
-def on_down_arrow(event):
-    if event.event_type == keyboard.KEY_DOWN:
+    elif move == "backward":
         move_backward()
-
-keyboard.on_press_key("left", on_left_arrow)
-keyboard.on_press_key("right", on_right_arrow)
-keyboard.on_press_key("up", on_up_arrow)
-keyboard.on_press_key("down", on_down_arrow)
-
-keyboard.on_release_key("left", power_off)
-keyboard.on_release_key("right", power_off)
-keyboard.on_release_key("up", power_off)
-keyboard.on_release_key("down", power_off)
-
-keyboard.wait("esc")
-
-power_off()
+    power_off()
