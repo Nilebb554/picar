1dc6d132691b91955bc2f3cf499689a42be136e4
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-08 17:51:54 +0100

    Update robot_controller.py

--- practice-scripts/robot_controller.py
+++ practice-scripts/robot_controller.py
@@ -3,11 +3,9 @@ import time
 
 robot = Robot(left=(19, 21), right=(24, 26))
 
-try:
-    while True:
-        robot.left
-        time.sleep(10)
-        robot.stop
-        time.sleep(1)
-
+robot.left()
+time.sleep(10)
+robot.right()
+time.sleep(10)
 robot.stop()
+time.sleep(1)
