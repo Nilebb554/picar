e6a95540a2b85fae0ff34c6cdb7829d3505ec13a
Abdinasir <112059604+Abdinasir1234@users.noreply.github.com>
2023-11-08 13:12:02 +0000

    klar med js

--- static/steering.js
+++ static/steering.js
@@ -1,26 +1,39 @@
-const keyState={
+const keyState = {
     "forward": 0,
-    "backward":0,
-    "left":0,
-    "right":0
-} 
+    "backward": 0,
+    "left": 0,
+    "right": 0
+};
 
-if(keyisdown){
-    if (key = forwardArrow){
-        "forward" = 1
+document.addEventListener("keydown", function(event) {
+    if (event.key === forwardArrow) {
+        keyState["forward"] = 1;
     }
+    if (event.key === backwardArrow) {
+        keyState["backward"] = 1;
+    }
+    if (event.key === leftArrow) {
+        keyState["left"] = 1;
+    }
+    if (event.key === rightArrow) {
+        keyState["right"] = 1;
+    }
+    console.log(keyState);
+});
 
-    if (key = backwardArrow){
-        "backward" = 1
+document.addEventListener("keyup", function(event) {
+    if (event.key === forwardArrow) {
+        keyState["forward"] = 0;
     }
-    
-    if (key = leftArrow){
-        "left" = 1
+    if (event.key === backwardArrow) {
+        keyState["backward"] = 0;
     }
+    if (event.key === leftArrow) {
+        keyState["left"] = 0;
+    }
+    if (event.key === rightArrow) {
+        keyState["right"] = 0;
+    }
+});
 
-    if (key = rightArrow){
-        "right" = 1
-    }
-
-}
 
