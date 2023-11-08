c695c2f58739bc1e7fe4bb79ce71b86733d5a58d
Abdinasir <112059604+Abdinasir1234@users.noreply.github.com>
2023-11-08 12:58:27 +0000

    Stardet with the script

---
+++ static/steering.js
@@ -0,0 +1,26 @@
+const keyState={
+    "forward": 0,
+    "backward":0,
+    "left":0,
+    "right":0
+} 
+
+if(keyisdown){
+    if (key = forwardArrow){
+        "forward" = 1
+    }
+
+    if (key = backwardArrow){
+        "backward" = 1
+    }
+    
+    if (key = leftArrow){
+        "left" = 1
+    }
+
+    if (key = rightArrow){
+        "right" = 1
+    }
+
+}
+
