c3002fc265e6829393d802957960f109cb644d40
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-08 16:46:34 +0100

    Update robot_controller.py

--- practice-scripts/robot_controller.py
+++ practice-scripts/robot_controller.py
@@ -1,29 +1,13 @@
-import socketio
+from gpiozero import Robot
+import time
 
-sio = socketio.Server()
-app = socketio.WSGIApp(sio) #run with gunicorn --threads 50 robot_controller:app maybe
-robot_state = {
-    "left": 0,
-    "right": 0,
-    "forward": 0,
-    "backward": 0
-}
+robot = Robot(left=(19, 21), right=(24, 26))
 
-@sio.event
-def connect(sid, environ):
-    print(sid, "connected")
+try:
+    while True
+        robot.left
+        time.sleep(10)
+        robot.stop
+        time.sleep(1)
 
-@sio.event
-def connect_error(sid):
-    print(sid, "connection failed")
-
-@sio.event
-def disconnect(sid):
-    print(sid, "disconnected")
-
-
-@sio.on("get_robot_state")
-def get_robot_state(sid, data):
-    global robot_state
-    robot_state = data
-    print(robot_state)
+robot.stop()
