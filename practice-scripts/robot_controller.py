e2bef105d45e077fcc85ea14533afc6e76a61820
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-09 17:42:47 +0100

    Update robot_controller.py

--- practice-scripts/robot_controller.py
+++ practice-scripts/robot_controller.py
@@ -3,9 +3,17 @@ import time
 
 robot = Robot(left=(19, 21), right=(24, 26))
 
-robot.left()
-time.sleep(10)
-robot.right()
-time.sleep(10)
+# Move backward
+robot.backward()
+time.sleep(5)  # Adjust the sleep duration as needed
+
+# Stop for a moment
 robot.stop()
 time.sleep(1)
+
+# Move forward
+robot.forward()
+time.sleep(5)  # Adjust the sleep duration as needed
+
+# Stop at the end
+robot.stop()
