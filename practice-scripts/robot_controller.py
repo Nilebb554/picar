4a39615f4494a6a1b9bb49eb98f09a2ae872a79a
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-09 17:47:02 +0100

    Update robot_controller.py

--- practice-scripts/robot_controller.py
+++ practice-scripts/robot_controller.py
@@ -3,9 +3,41 @@ import time
 
 robot = Robot(left=(19, 21), right=(24, 26))
 
-# Move forward
-robot.forward()
-time.sleep(5)
+def move_forward(speed=0.5, duration=1):
+    robot.forward(speed)
+    time.sleep(duration)
+    robot.stop()
 
-# Stop
-robot.stop()
+def move_backward(speed=0.5, duration=1):
+    robot.backward(speed)
+    time.sleep(duration)
+    robot.stop()
+
+def move_left(speed=0.5, duration=1):
+    robot.left(speed)
+    time.sleep(duration)
+    robot.stop()
+
+def move_right(speed=0.5, duration=1):
+    robot.right(speed)
+    time.sleep(duration)
+    robot.stop()
+
+try:
+    while True:
+        move = input("Where to move (forward, backward, left, right, stop): ")
+        if move == "forward":
+            move_forward()
+        elif move == "backward":
+            move_backward()
+        elif move == "left":
+            move_left()
+        elif move == "right":
+            move_right()
+        elif move == "stop":
+            robot.stop()
+except KeyboardInterrupt:
+    print("Stopping the robot")
+    robot.stop()
+finally:
+    robot.stop()
