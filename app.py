74ec34bb5b3f602d25ade9e61090f307515b06d7
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-14 17:15:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def handle_keyState(keyState):
 
 if __name__ == "__main__":
     try:
-        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+        socketio.run(app, host='0.0.0.0', port=3000, debug=True)
     finally:
         if callable(power_down):
             power_down()
