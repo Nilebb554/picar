2d828ddb37bf49aef2c3b6a9f87ac5c5c5cc24b0
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-14 16:41:11 +0000

    possible fix?

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+try:
+    import RPi.GPIO as GPIO
+    from control import change_state, power_up, power_down
+except (RuntimeError, ImportError) as e:
+    print(f"Error importing control module: {e}")
+    change_state = None
+    power_up = None
+    power_down = None
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -10,15 +18,27 @@ def hello_world():
 
 @socketio.on("connect")
 def connect():
+    if callable(power_up):
+        power_up()
     print("Client connected")
 
 @socketio.on("disconnect")
 def disconnect():
+    if callable(power_down):
+        power_down()
     print("Client disconnected")
 
 @socketio.on("keyState")
 def handle_keyState(keyState):
-    print(keyState["forward"])
+    print(keyState)
+    try:
+        change_state(keyState)
+    except:
+        print(keyState)
 
 if __name__ == "__main__":
-    socketio.run(app)
+    try:
+        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    finally:
+        if callable(power_down):
+            power_down()
