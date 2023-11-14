d3e3e27438d636fb93bc1b442c36f37f727cb576
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-14 17:19:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def handle_keyState(keyState):
     change_state(keyState)
 
 if __name__ == "__main__":
-        socketio.run(app, host='0.0.0.0', port=3000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
