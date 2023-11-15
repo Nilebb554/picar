270a909f9189f3143ea7f30c78ac0237bb529b52
Abdinasir <112059604+Abdinasir1234@users.noreply.github.com>
2023-11-15 14:14:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
+
 from flask_socketio import SocketIO
 try:
     import RPi.GPIO as GPIO
@@ -11,10 +13,29 @@ except (RuntimeError, ImportError) as e:
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @socketio.on("connect")
 def connect():
