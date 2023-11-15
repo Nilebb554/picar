29abe45085f34b6bf24526e96de0e505d8fe99ec
Abdinasir <112059604+Abdinasir1234@users.noreply.github.com>
2023-11-15 13:08:11 +0000

    fixat code

--- app.py
+++ app.py
@@ -5,34 +5,21 @@ import cv2
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-camera = None  # Initialize the camera as None
-
-def init_camera():
-    global camera
-    try:
-        camera = cv2.VideoCapture(0)
-        # Additional camera initialization code if needed
-    except:
-        print("Error initializing camera")
+camera=cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
-        if camera:
-            # If the camera is initialized, read frames
-            success, frame = camera.read()
-            if not success:
-                break
-            else:
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
         else:
-            # If the camera is not available, create a placeholder frame
-            frame = b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x01\x00\x96\x00\x96\x00\x00'
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/')
 def hello_world():
     return render_template("index.html")
@@ -51,7 +38,5 @@ def disconnect():
     print("Client disconnected")
 
 
-
 if __name__ == "__main__":
-    init_camera()  # Initialize the camera before running the app
     socketio.run(app)
