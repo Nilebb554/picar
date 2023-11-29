fb6a2f0d6c501c055a218f98745e064a32fe6ffc
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-29 15:27:27 +0000

    revert

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from flask import Flask, render_template, Response
+import cv2
+
 from flask_socketio import SocketIO
 import RPi.GPIO as GPIO
 from control import change_state, power_up, power_down
-from camera_pi import Camera
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @socketio.on("connect")
 def connect():
@@ -26,18 +46,5 @@ def handle_keyState(keyState):
     print(keyState)
     change_state(keyState)
 
-def gen(camera):
-    while True: 
-        frame= camera.get_frame()
-        yield (b'--frame\r\n' 
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    vs.release()
-    cv2.destroyAllWindows()
-
-@app.route('/video_feed')
-def video_feed(): 
-   return Response(gen(Camera()), 
-                    mimetype='multipart/x-mixed-replace; boundary=frame') 
-
 if __name__ == "__main__":
-    socketio.run(app,host="0.0.0.0", port=5000, debug=True, threaded=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
