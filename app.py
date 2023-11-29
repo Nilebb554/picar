1bb2ea4bab41133259d352090caf1b924e55a038
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-29 13:15:36 +0000

    fix?

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from camera_opencv import generate_frames
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-camera = cv2.VideoCapture(0)
+camera = PiCamera()
 
 @app.route('/')
 def hello_world():
@@ -33,6 +33,29 @@ def disconnect():
 def handle_keyState(keyState):
     print(keyState)
     change_state(keyState)
+
+def generate_frames():
+    while True:
+        # Capture an image from the PiCamera
+        frame = capture_frame()
+
+        # Encode the frame as JPEG
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+
+        # Yield the frame in the required format
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+def capture_frame():
+    # Capture an image from the PiCamera
+    frame = None
+    with PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.capture('/tmp/frame.jpg')  # Capture to a temporary file
+        frame = cv2.imread('/tmp/frame.jpg')
+
+    return frame
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
