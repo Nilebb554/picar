d5f81331f8869de38984dd2c9716c5f364e05d35
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-11-29 15:19:46 +0000

    backup if all code not works

---
+++ old_app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, Response
+import cv2
+
+from flask_socketio import SocketIO
+import RPi.GPIO as GPIO
+from control import change_state, power_up, power_down
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@socketio.on("connect")
+def connect():
+    power_up()
+    print("Client connected")
+
+@socketio.on("disconnect")
+def disconnect():
+    power_down()
+    print("Client disconnected")
+
+@socketio.on("keyState")
+def handle_keyState(keyState):
+    print(keyState)
+    change_state(keyState)
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
