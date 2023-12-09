3d19411b39908673f17b67421c78b6c82098bdc6
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-12-09 12:07:10 +0000

    changed it to cv2 maybe problem with picamera and os?

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from flask_socketio import SocketIO
 import RPi.GPIO as GPIO
 from control import change_state, power_up, power_down
 
-from picamera import PiCamera
-import io
+import cv2
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 camera = PiCamera()
 
 def generate_frames():
-    while True:
-        stream = io.BytesIO()
-        camera.capture(stream, format="jpeg")   
-        frame=stream.getvalue()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    stream = cv2.VideoCapture(0)
+    try:
+        while True:
+            frame=stream.read()
+            jpeg = cv2.imencode('.jpg', frame)
+            frame=jpeg.tobytes()
+            yield(b'--frame\r\n'
+                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    finally:
+        stream.release()
+        cv2.destroyAllWindows()
 
 @app.route('/')
 def hello_world():
