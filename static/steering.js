2884db3c0de673622ac1cc8cae2e5e2d30e228a7
Abdinasir <112059604+Abdinasir1234@users.noreply.github.com>
2023-12-13 12:12:40 +0000

    sockit io fixed

--- static/steering.js
+++ static/steering.js
@@ -5,34 +5,37 @@ const keyState = {
     "right": 0
 };
 
+let keyAlreadyPressed = false;
+
 document.addEventListener("keydown", function(event) {
+    if (keyAlreadyPressed) return;
+    keyAlreadyPressed = true;
+
     if (event.key === "ArrowUp") {
         keyState["forward"] = 1;
-    }
-    if (event.key === "ArrowDown") {
+    } else if (event.key === "ArrowDown") {
         keyState["backward"] = 1;
-    }
-    if (event.key === "ArrowLeft") {
+    } else if (event.key === "ArrowLeft") {
         keyState["left"] = 1;
-    }
-    if (event.key === "ArrowRight") {
+    } else if (event.key === "ArrowRight") {
         keyState["right"] = 1;
     }
+
     console.log(keyState);
 });
 
 document.addEventListener("keyup", function(event) {
+    keyAlreadyPressed = false;
+
     if (event.key === "ArrowUp") {
         keyState["forward"] = 0;
-    }
-    if (event.key === "ArrowDown") {
+    } else if (event.key === "ArrowDown") {
         keyState["backward"] = 0;
-    }
-    if (event.key === "ArrowLeft") {
+    } else if (event.key === "ArrowLeft") {
         keyState["left"] = 0;
-    }
-    if (event.key === "ArrowRight") {
+    } else if (event.key === "ArrowRight") {
         keyState["right"] = 0;
     }
+
     console.log(keyState);
 });
