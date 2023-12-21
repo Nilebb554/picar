4a875dbafae9df904cadfbbc60d6e75d37153f9e
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-12-21 12:17:50 +0000

    fixed

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
+import io
+import time
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
