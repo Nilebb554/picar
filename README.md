685bf013cd73a63d9556b5d9dfd818a63e843b40
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2023-12-21 12:49:12 +0000

    merge this with main

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# picar
-Internet PiCar
-To create local venv
-"python -m venv venv"
+# picar with picamera
 
-To start venv: "source venv/bin/activate", when in flask_app.
-This will make it so that the same version of dependencies are used.
+# Setup virtuall envirement
+1. python -m venv venv --system-site-packages
+2. pip install -r requirements.txt
+3. source venv/bin/activate
 
-To install all dependencies run: 
-"pip install -r requirements.txt"
-
-
-To run flask app
-cd into venv. type "flask run".
-
-
-git pull origin main
+# To start
+1. flask run --host=your_ip
--- app.py
+++ app.py
@@ -1,32 +1,41 @@
 from flask import Flask, render_template,Response
 from flask_socketio import SocketIO
 
-import cv2
 import RPi.GPIO as GPIO
 from control import change_state, power_up, power_down
 
+from threading import Condition
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 
 import io
 import time
 
-import io
-import time
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-camera=cv2.VideoCapture(0)
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+picam2 = None
+CameraOutput = StreamingOutput()
 
 def generate_frames():
     while True:
-        success,frame=camera.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        with cameraOutput.condition:
+            cameraOutput.condition.wait()
+            frame = cameraOutput.frame
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
  
 @app.route('/')
 def hello_world():
@@ -34,6 +43,15 @@ def hello_world():
 
 @app.route('/video_feed')
 def video_feed():
+    global picam2
+    global cameraOutput
+
+    if picam2 is None:
+        picam2 = Picamera2()
+        picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+        cameraOutput = StreamingOutput()
+        picam2.start_recording(JpegEncoder(), FileOutput(cameraOutput))
+    
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @socketio.on("connect")
