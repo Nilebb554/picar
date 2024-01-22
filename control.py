421be9b6626cab148bcae6bb7abafa8be91e55af
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2024-01-22 15:38:05 +0000

    tankstyrnigen var inte korrekt. Har fixat den med komplex matte...

--- control.py
+++ control.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import math
 import time
 
 LF = 26
@@ -32,17 +33,42 @@ def power_down():
     GPIO.cleanup()
 
 def calculate_motor_speeds(x, y): 
-    if x == 0:
-        left_speed = y * 100
-        right_speed = y * 100
-    else:
-        left_speed = y * 100 + x * 100
-        right_speed = y * 100 - x * 100
-    
-    left_speed = max(min(left_speed, 100), -100)
-    right_speed = max(min(right_speed, 100), -100)
+    import math
 
-    return left_speed, right_speed
+def tank_drive(x, y):
+    if x == 0 and y == 0:
+        left_speed = 0
+        right_speed = 0
+    else:
+        z = math.sqrt(x * x + y * y)
+        rad = math.acos(math.fabs(x) / z)
+        angle = rad * 180 / math.pi
+
+        tcoeff = -1 + (angle / 90) * 2
+        turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
+        turn = round(turn * 100, 0) / 100
+
+        mov = max(math.fabs(y), math.fabs(x))
+
+        if (x >= 0 and y >= 0) or (x < 0 and y < 0):
+            left_speed = mov
+            right_speed = turn
+        else:
+            right_speed = mov
+            left_speed = turn
+
+        if y < 0:
+            left_speed = 0 - left_speed
+            right_speed = 0 - right_speed
+
+        left_speed = max(min(left_speed, 100), -100)
+        right_speed = max(min(right_speed, 100), -100)
+
+    print("Right Speed:", right_speed*100)
+    print("Left Speed:", left_speed*100)
+
+
+    return left_speed*100, right_speed*100
 
 def change_motor_speeds(state):
     #convert it to number between -100 and 100
@@ -57,6 +83,7 @@ def change_motor_speeds(state):
     else:
         lf_pwm.ChangeDutyCycle(0)
         lb_pwm.ChangeDutyCycle(0)
+
     if right_speed > 0:
         rf_pwm.ChangeDutyCycle(right_speed)
         rb_pwm.ChangeDutyCycle(0)
--- practice-scripts/tanktest.py
+++ practice-scripts/tanktest.py
@@ -1,19 +1,37 @@
+import math
+
 def tank_drive(x, y):
-    if x == 0:
-        left_speed = y * 100
-        right_speed = y * 100
+    if x == 0 and y == 0:
+        left_speed = 0
+        right_speed = 0
     else:
-        left_speed = y * 100 + x * 100
-        right_speed = y * 100 - x * 100
-    
-    left_speed = max(min(left_speed, 100), -100)
-    right_speed = max(min(right_speed, 100), -100)
-    
-    print(right_speed, left_speed)
+        z = math.sqrt(x * x + y * y)
+        rad = math.acos(math.fabs(x) / z)
+        angle = rad * 180 / math.pi
 
+        tcoeff = -1 + (angle / 90) * 2
+        turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
+        turn = round(turn * 100, 0) / 100
 
+        mov = max(math.fabs(y), math.fabs(x))
+
+        if (x >= 0 and y >= 0) or (x < 0 and y < 0):
+            left_speed = mov
+            right_speed = turn
+        else:
+            right_speed = mov
+            left_speed = turn
+
+        if y < 0:
+            left_speed = 0 - left_speed
+            right_speed = 0 - right_speed
+
+        left_speed = max(min(left_speed, 100), -100)
+        right_speed = max(min(right_speed, 100), -100)
+
+    print("Right Speed:", right_speed)
+    print("Left Speed:", left_speed)
 
 x = -1
 y = 0
 tank_drive(x, y)
-
