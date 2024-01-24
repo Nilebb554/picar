614bee3a3ed7f1b30e31861f87236ceb45d2471c
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2024-01-24 12:41:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,18 +56,18 @@ def video_feed():
 
 @socketio.on("connect")
 def connect():
-    #power_up()
+    power_up()
     print("Client connected")
 
 @socketio.on("disconnect")
 def disconnect():
-    #power_down()
+    power_down()
     print("Client disconnected")
 
 @socketio.on("keyState")
 def handle_keyState(keyState):
     print(keyState)
-    #change_state(keyState)
+    change_state(keyState)
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
