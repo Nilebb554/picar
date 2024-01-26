9e7044cb0e5f626b834ef62009773d0533fc4d1c
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2024-01-26 10:07:19 +0100

    Update steering.js

--- static/steering.js
+++ static/steering.js
@@ -21,16 +21,16 @@ document.addEventListener("keydown", function(event) {
     if (!keyDown[event.key]) {
         keyDown[event.key] = true;
         if (event.key === "ArrowUp") {
-            keyState["y"] += 1;
+            keyState["y"] = 1;
         }
         if (event.key === "ArrowDown") {
-            keyState["y"] -= 1;
+            keyState["y"] = -1;
         }
         if (event.key === "ArrowLeft") {
-            keyState["x"] -= 1;
+            keyState["x"] = -1;
         }
         if (event.key === "ArrowRight") {
-            keyState["x"] += 1;
+            keyState["x"] = 1;
         }
         console.log(keyState);
         socket.emit("keyState", keyState);
@@ -41,16 +41,16 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keyup", function(event) {
     keyDown[event.key] = false;
     if (event.key === "ArrowUp") {
-        keyState["y"] -= 1;
+        keyState["y"] = -1;
     }
     if (event.key === "ArrowDown") {
-        keyState["y"] += 1;
+        keyState["y"] = 1;
     }
     if (event.key === "ArrowLeft") {
-        keyState["x"] += 1;
+        keyState["x"] = 1;
     }
     if (event.key === "ArrowRight") {
-        keyState["x"] -= 1;
+        keyState["x"] = -1;
     }
     console.log(keyState);
     socket.emit("keyState", keyState);
