97457d41fb296be840824dc62f9c8747e3c180db
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2024-02-14 14:23:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,65 +13,67 @@ import libcamera
 import io
 import time
 
-
-app = Flask(__name__)
-socketio = SocketIO(app)
-
-class StreamingOutput(io.BufferedIOBase):
-    def __init__(self):
-        self.frame = None
-        self.condition = Condition()
-
-    def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
-            
-picam2 = None
-CameraOutput = StreamingOutput()
-
-def generate_frames():
-    while True:
-        with cameraOutput.condition:
-            cameraOutput.condition.wait()
-            frame = cameraOutput.frame
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
- 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
-
-@app.route('/video_feed')
-def video_feed():
-    global picam2
-    global cameraOutput
-
-    if picam2 is None:
-        picam2 = Picamera2()
-        camera_config = picam2.create_video_configuration(main={"size": (1200, 600)})
-        camera_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
-        picam2.configure(camera_config)
-        cameraOutput = StreamingOutput()
-        picam2.start_recording(JpegEncoder(), FileOutput(cameraOutput))
+def create_app():
+    app = Flask(__name__)
+    socketio = SocketIO(app)
     
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@socketio.on("connect")
-def connect():
-    power_up()
-    print("Client connected")
-
-@socketio.on("disconnect")
-def disconnect():
-    power_down()
-    print("Client disconnected")
-
-@socketio.on("keyState")
-def handle_keyState(keyState):
-    print(keyState)
-    change_motor_speeds(keyState)
+    class StreamingOutput(io.BufferedIOBase):
+        def __init__(self):
+            self.frame = None
+            self.condition = Condition()
     
+        def write(self, buf):
+            with self.condition:
+                self.frame = buf
+                self.condition.notify_all()
+                
+    picam2 = None
+    CameraOutput = StreamingOutput()
     
+    def generate_frames():
+        while True:
+            with cameraOutput.condition:
+                cameraOutput.condition.wait()
+                frame = cameraOutput.frame
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+     
+    @app.route('/')
+    def hello_world():
+        return render_template("index.html")
+    
+    @app.route('/video_feed')
+    def video_feed():
+        global picam2
+        global cameraOutput
+    
+        if picam2 is None:
+            picam2 = Picamera2()
+            camera_config = picam2.create_video_configuration(main={"size": (1200, 600)})
+            camera_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
+            picam2.configure(camera_config)
+            cameraOutput = StreamingOutput()
+            picam2.start_recording(JpegEncoder(), FileOutput(cameraOutput))
+        
+        return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+    @socketio.on("connect")
+    def connect():
+        power_up()
+        print("Client connected")
+    
+    @socketio.on("disconnect")
+    def disconnect():
+        power_down()
+        print("Client disconnected")
+    
+    @socketio.on("keyState")
+    def handle_keyState(keyState):
+        print(keyState)
+        change_motor_speeds(keyState)
+        
+        
 if __name__ == "__main__":
+    app = create_app()
+    socketio = SocketIO(app)
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
