48ac6380bf1e7137f9203c76d0304fc59e5eb1bd
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2024-03-11 13:23:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from control import change_motor_speeds, power_up, power_down
 
 from threading import Condition
 from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 import libcamera
 
 import io
