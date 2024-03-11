f4440eddee62a10dc57d4a46455769fbc0f46f1f
Nilebb554 <111964317+Nilebb554@users.noreply.github.com>
2024-03-11 13:57:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ def disconnect():
     print("Client disconnected")
     
 @socketio.on("steering_data")
-def handle_keyState(steeringData):
-    change_motor_speeds(steeringData)
+def handle_keyState(data):
+    change_motor_speeds(data)
         
         
 if __name__ == "__main__":
